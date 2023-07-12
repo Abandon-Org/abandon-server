4bfefea53953fa7661b182bce16ee30ca666ed08
yycxx <yyi99yy@163.com>
2023-07-12 16:40:59 +0800

    注册登录写反了

--- src/app/routes/auth/user.py
+++ src/app/routes/auth/user.py
@@ -9,11 +9,11 @@ router = APIRouter(prefix="/auth")
 # router注册的函数都会自带/auth，所以url是/auth/register
 @router.post("/register")
 async def register():
-    usr_info: dict = {'这是一个usr': 1}
-    expire, token = AbandonJWT.get_token(usr_info)
-    return AbandonJSONResponse.success(dict(token=token, expire=expire, usr_info=usr_info))
+    return '注册！'
 
 
 @router.post("/login")
 async def login():
-    return "登陆成功"
+    usr_info: dict = {'这是一个usr': 1}
+    expire, token = AbandonJWT.get_token(usr_info)
+    return AbandonJSONResponse.success(dict(token=token, expire=expire, usr_info=usr_info))
