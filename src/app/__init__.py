f58e605d04a268a7a79e8f57375fb21e80e9db6d
yycxx <yyi99yy@163.com>
2023-07-12 15:26:31 +0800

    解决后端跨域问题

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.app.routes.auth import user
+
+
 
 abandon = FastAPI()
 
 
 # 注册路由
 abandon.include_router(user.router)
+
+abandon.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
